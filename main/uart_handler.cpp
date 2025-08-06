439cbdc3f563dfb5f023ff66f2a26a06ab018fb9
Jackson Ming Hu <huming2207@gmail.com>
2025-08-06 12:36:04 +1000

    uart_handler: implement screen

--- main/uart_handler.cpp
+++ main/uart_handler.cpp
@@ -1,7 +1,10 @@
+#include <cstring>
+#include <string>
 #include <driver/uart.h>
 #include <driver/gpio.h>
 #include <esp_log.h>
 #include "uart_handler.hpp"
+#include "esp_lvgl_port.h"
 
 esp_err_t uart_handler::init()
 {
@@ -15,9 +18,10 @@ esp_err_t uart_handler::init()
     uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
     uart_config.source_clk = UART_SCLK_APB;
 
-    auto ret = uart_driver_install(port, 8192, 8192, 32, &uart_evt_queue, 0);
+    auto ret = uart_driver_install(port, 512, 512, 32, &uart_evt_queue, 0);
     ret = ret ?: uart_param_config(port, &uart_config);
     ret = ret ?: uart_set_pin(port, GPIO_NUM_NC, GPIO_NUM_9, GPIO_NUM_NC, GPIO_NUM_NC);
+    ret = ret ?: uart_enable_pattern_det_baud_intr(port, '>', 3, 9, 0, 0);
 
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "UART setup failed");
@@ -30,7 +34,8 @@ esp_err_t uart_handler::init()
 
 void uart_handler::uart_evt_task_func(void *_ctx)
 {
-    auto *ctx = (uart_handler *)_ctx;
+    auto *ctx = static_cast<uart_handler *>(_ctx);
+    uint8_t rx_buf[1024] = { 0 };
     while (true) {
         uart_event_t event = {};
         if (xQueueReceive(ctx->uart_evt_queue, &event, portMAX_DELAY) != pdTRUE) {
@@ -39,16 +44,51 @@ void uart_handler::uart_evt_task_func(void *_ctx)
         }
 
         switch (event.type) {
-            case UART_DATA: {
-                // TODO read here
-                break;
-            }
-
             case UART_FIFO_OVF:
             case UART_BUFFER_FULL:
             case UART_PARITY_ERR:
             case UART_FRAME_ERR: {
                 uart_flush(ctx->port);
+                xQueueReset(ctx->uart_evt_queue);
+                break;
+            }
+
+            case UART_PATTERN_DET: {
+                int pos = uart_pattern_pop_pos(ctx->port);
+                if (pos == -1 || pos > sizeof(rx_buf)) {
+                    // Invalid pattern position
+                    uart_flush_input(ctx->port);
+                    ESP_LOGE(TAG, "uart_read: got garbage data?");
+                    return;
+                }
+
+                int preamble_len = pos + 3;
+                int read_len = uart_read_bytes(ctx->port, rx_buf, preamble_len, pdMS_TO_TICKS(100));
+
+                if (read_len != preamble_len) {
+                    ESP_LOGW(TAG, "Unexpected preamble read length");
+                    uart_flush_input(ctx->port);
+                    break;
+                }
+
+                memset(rx_buf, 0, sizeof(rx_buf));
+
+                int payload_len = 0;
+                while (payload_len < sizeof(rx_buf) - 1) {
+                    uint8_t byte = 0;
+                    int len = uart_read_bytes(ctx->port, &byte, 1, pdMS_TO_TICKS(50));
+                    if (len > 0) {
+                        if (byte == '\r') break;
+                        rx_buf[payload_len++] = byte;
+                    } else {
+                        break; // Timeout
+                    }
+                }
+
+                rx_buf[payload_len] = '\0';
+                ctx->handle_uart_read(rx_buf, payload_len);
+
+                memset(rx_buf, 0, sizeof(rx_buf));
                 break;
             }
 
@@ -58,4 +98,90 @@ void uart_handler::uart_evt_task_func(void *_ctx)
 
         }
     }
+}
+
+void uart_handler::handle_uart_read(uint8_t *buf, const size_t len)
+{
+    auto uart_str = std::string((char *)buf, len);
+
+    size_t comma_pos = uart_str.find(',');
+    if (comma_pos == std::string_view::npos) {
+        screen_draw_error();
+    }
+
+    std::string rssi = uart_str.substr(0, comma_pos);
+    std::string snr = uart_str.substr(comma_pos + 1);
+
+    screen_draw_readings(rssi, snr);
+}
+
+void uart_handler::screen_draw_error()
+{
+    lvgl_port_lock(pdMS_TO_TICKS(100));
+
+    if (base_obj != nullptr) {
+        lv_obj_delete(base_obj);
+    }
+
+    base_obj = lv_obj_create(lv_scr_act());
+    if (base_obj == nullptr) {
+        ESP_LOGE(TAG, "Can't draw base object!!");
+        return;
+    }
+
+    lv_obj_set_size(base_obj, 240, 240);
+    lv_obj_set_scrollbar_mode(base_obj, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_align(base_obj, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+    lv_obj_set_style_bg_opa(base_obj, 0, 0);
+    lv_obj_set_style_border_width(base_obj, 0, 0);
+    lv_obj_set_style_radius(base_obj, 0, 0);
+    lv_obj_set_style_pad_all(base_obj, 0, 0);
+    lv_obj_set_style_outline_width(base_obj, 0, 0);
+
+    lv_obj_t *obj = lv_label_create(base_obj);
+    lv_obj_set_pos(obj, LV_PCT(15), LV_PCT(30));
+    lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_style_text_font(obj, &lv_font_montserrat_36, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text(obj, "ERROR!!!");
+
+    lvgl_port_unlock();
+}
+
+void uart_handler::screen_draw_readings(const std::string &rssi, const std::string &snr)
+{
+    lvgl_port_lock(pdMS_TO_TICKS(100));
+
+    if (base_obj != nullptr) {
+        lv_obj_delete(base_obj);
+    }
+
+    base_obj = lv_obj_create(lv_scr_act());
+    if (base_obj == nullptr) {
+        ESP_LOGE(TAG, "Can't draw base object!!");
+        return;
+    }
+
+    lv_obj_set_size(base_obj, 240, 240);
+    lv_obj_set_scrollbar_mode(base_obj, LV_SCROLLBAR_MODE_OFF);
+    lv_obj_align(base_obj, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+    lv_obj_set_style_bg_opa(base_obj, 0, 0);
+    lv_obj_set_style_border_width(base_obj, 0, 0);
+    lv_obj_set_style_radius(base_obj, 0, 0);
+    lv_obj_set_style_pad_all(base_obj, 0, 0);
+    lv_obj_set_style_outline_width(base_obj, 0, 0);
+
+
+    lv_obj_t *rssi_obj = lv_label_create(base_obj);
+    lv_obj_set_pos(rssi_obj, LV_PCT(3), LV_PCT(10));
+    lv_obj_set_size(rssi_obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_style_text_font(rssi_obj, &lv_font_montserrat_36, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text_fmt(rssi_obj, "RSSI: %s", rssi.c_str());
+
+    lv_obj_t *snr_obj = lv_label_create(base_obj);
+    lv_obj_set_pos(snr_obj, LV_PCT(3), LV_PCT(30));
+    lv_obj_set_size(snr_obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_style_text_font(snr_obj, &lv_font_montserrat_36, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_label_set_text_fmt(snr_obj, "SNR: %s", snr.c_str());
+
+    lvgl_port_unlock();
 }
--- main/uart_handler.hpp
+++ main/uart_handler.hpp
@@ -3,6 +3,8 @@
 #include <driver/uart.h>
 #include <esp_err.h>
 
+#include "misc/lv_types.h"
+
 class uart_handler
 {
 public:
@@ -20,13 +22,16 @@ private:
 
 public:
     esp_err_t init();
+    void handle_uart_read(uint8_t *buf, size_t len);
+    void screen_draw_error();
+    void screen_draw_readings(const std::string &rssi, const std::string &snr);
 
 private:
     static void uart_evt_task_func(void *_ctx);
 
-private:
     uart_port_t port = UART_NUM_1;
     QueueHandle_t uart_evt_queue = nullptr;
+    lv_obj_t *base_obj = nullptr;
 
 private:
     static const constexpr char TAG[] = "uart_handler";
